7fcf931d9c1917ad433d072fb6149610f99b4608
pdmashkov <pdmashkov@rolftech.ru>
2025-01-11 16:15:02 +0300

    add post api test

---
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldSendPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Что то там и зачем то еще")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Что то там и зачем то еще"));
+    }
+}
