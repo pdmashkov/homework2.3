88c26a43004229aae37d714c0557556fe9bb59c2
pdmashkov <pdmashkov@rolftech.ru>
2025-01-11 16:16:37 +0300

    Починил тест

--- src/test/java/ru/netology/post/PostmanEchoTest.java
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -16,6 +16,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Что то там и зачем то"));
+                .body("data", equalTo("Что то там и зачем то еще"));
     }
 }
